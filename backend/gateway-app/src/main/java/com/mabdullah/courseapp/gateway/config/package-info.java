ce796aacb5fa98c14a22d5c804c2fc6825ae0a83
Muhammad Abdullah <muhammad.abdullah.0913@gmail.com>
2025-05-16 21:21:09 -0400

    chore: restructured project to use /backend directory

---
+++ backend/gateway-app/src/main/java/com/mabdullah/courseapp/gateway/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Application configuration.
+ */
+package com.mabdullah.courseapp.gateway.config;
---
+++ backend/gateway-app/src/main/java/com/mabdullah/courseapp/gateway/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer visual models.
+ */
+package com.mabdullah.courseapp.gateway.web.rest.vm;
