450e49f4b31e223d6b601d3fa5a95ea23520a482
Muhammad Abdullah <muhammad.abdullah.0913@gmail.com>
2025-05-16 21:44:18 -0400

    Initial version of studentApp generated by generator-jhipster@8.11.0

---
+++ backend/student-app/src/main/java/com/mabdullah/courseapp/student/aop/logging/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Logging aspect.
+ */
+package com.mabdullah.courseapp.student.aop.logging;
---
+++ backend/student-app/src/main/java/com/mabdullah/courseapp/student/web/rest/errors/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer error handling.
+ */
+package com.mabdullah.courseapp.student.web.rest.errors;
